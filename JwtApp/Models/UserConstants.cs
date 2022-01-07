ce21e028ff49343b0df5cb77f490a6ae15986f93
MrWatelse1 <ogbodoprince@gmail.com>
2022-01-07 12:48:26 +0100

    second commit

---
+++ JwtApp/Models/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        { 
+            new UserModel(){ Username ="prince_admin", EmailAddress="ogbodoprince@gmail.com", Password="MyPass_w0rd", GivenName ="Prince", Surname ="Ogbodo", Role ="Administrator" },
+            new UserModel(){Username ="jason_seller", EmailAddress="jason.seller@email.com", Password="MyPass_w0rd", GivenName="Jason", Surname="Bryant", Role="Seller" },
+        };
+    }
+}
